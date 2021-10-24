e18f5bd8e8e74267add1565804d68f6cd923f649
Lohithaksh <83767927+lohithakshAFK@users.noreply.github.com>
2021-10-24 15:34:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ right_wrist_y = 0;
 left_wrist_x = 0;
 right_wrist_x = 0;
 song = "";
+score_left = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -15,22 +16,34 @@ function setup(){
     video.hide();
     pose = ml5.poseNet(video,modelLoaded);
     pose.on("pose",gotPoses)
-}
+};
 
 function modelLoaded(){
     console.log("....................")
-}
+};
 
 function draw(){
     image(video,0,0,500,500);
+    stroke("red");
+    fill("red");
+    if (score_left > 0.2){
+    circle(left_wrist_x,left_wrist_y,20);
+    number_left = Number(left_wrist_y);
+    number_left_left = floor(number_left);
+    volume = number_left_left / 500;
+    document.getElementById("volume").innerHTML = "Volume : " + volume;
+    //sikodjsjfksfkdhfkdsjjsojfosdjfdjijdiujsdiuhsaudhuihdugygdgdsug38yd837yr8436r46yyfd
+    song.setVolume(volume);
+    }
+    
 
-}
+};
 
 function play(){
     song.play();
     song.setVolume(1);
     song.rate(1);
-}
+};
 
 function gotPoses(results){
     if (results.length > 0){
@@ -41,5 +54,8 @@ function gotPoses(results){
           right_wrist_y = results[0].pose.rightWrist.y;
           console.log("Position of right wrist is x = " + right_wrist_x + " y = " + right_wrist_y);
           console.log("Position of left wrist is x = " + left_wrist_x + " y = " + left_wrist_y);
-    }
-}
+
+          score_left = results[0].pose.keypoints[9].score;
+          console.log("score = " + score_left);
+    };
+};
