63e8f7966e03c55f893ec034dd92b6db5f51be54
Lohithaksh <83767927+lohithakshAFK@users.noreply.github.com>
2021-11-08 16:19:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ right_wrist_y = 0;
 left_wrist_x = 0;
 right_wrist_x = 0;
 song = "";
+
 score_left = 0;
 
 function preload(){
@@ -26,17 +27,37 @@ function draw(){
     image(video,0,0,500,500);
     stroke("red");
     fill("red");
+
+    circle(right_wrist_x,right_wrist_y,20);
+    if(right_wrist_y > 0 && right_wrist_y <= 100){
+        document.getElementById("speed").innerHTML = "Speed: 0.5x";
+        song.rate(0.5);
+    }
+    else if(right_wrist_y > 100 && right_wrist_y <= 200){
+        document.getElementById("speed").innerHTML = "Speed: 1x";
+        song.rate(1);
+    }
+    else if(right_wrist_y > 200 && right_wrist_y <= 300){
+        document.getElementById("speed").innerHTML = "Speed: 1.5x";
+        song.rate(1.5);
+    }
+    else if(right_wrist_y > 300 && right_wrist_y <= 400){
+        document.getElementById("speed").innerHTML = "Speed: 2x";
+        song.rate(2);
+    }    
+    else if(right_wrist_y > 400 && right_wrist_y <=500){
+        document.getElementById("speed").innerHTML = "Speed: 2.5x";
+        song.rate(2.5);
+    }
+
     if (score_left > 0.2){
     circle(left_wrist_x,left_wrist_y,20);
     number_left = Number(left_wrist_y);
     number_left_left = floor(number_left);
     volume = number_left_left / 500;
     document.getElementById("volume").innerHTML = "Volume : " + volume;
-    //sikodjsjfksfkdhfkdsjjsojfosdjfdjijdiujsdiuhsaudhuihdugygdgdsug38yd837yr8436r46yyfd
     song.setVolume(volume);
     }
-    
-
 };
 
 function play(){
